2ce2d92e80af41d493a33bcb439781a536a7340e
Layssa-1 <132698476+Layssa-1@users.noreply.github.com>
2023-08-04 17:22:11 +0000

    atualizar

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import express from "express";      // Requisição do pacote do express
 const app = express();              // Instancia o Express
 const port = 3000;                  // Define a porta
+//index.js
+import dotenv from "dotenv";
+
+dotenv.config();
+import { selectUsuarios } from "./bd.js";
+app.get("/usuarios", async (req, res) => {
+  console.log("Rota GET/usuarios solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+});
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   res.json({
